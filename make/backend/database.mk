e2282c25324003ccc9bc35195bcd1f137423c038
Jonathan Houze <jonathan.houze@gmail.com>
2025-10-12 10:56:47 +0800

    fix: makefile mistakes

--- make/backend/database.mk
+++ make/backend/database.mk
@@ -26,7 +26,7 @@ db-create-container-%:
 .PHONY: db-migrate
 db-migrate: $(addprefix db-migrate-,$(DB_TARGETS))
 
-## db/db-migrate-container-<target>: migrate specific database
+## db/db-migrate-<target>: migrate specific database
 .PHONY: db-migrate-%
 db-migrate-%:
 	@$(MAKE) _db-migrate DB_TARGET=$*
@@ -35,12 +35,12 @@ db-migrate-%:
 .PHONY: db-reset
 db-reset: $(addprefix db-reset-,$(DB_TARGETS))
 
-## db/db-reset-container-<target>: reset specific database
+## db/db-reset-<target>: reset specific database
 .PHONY: db-reset-%
 db-reset-%:
 	@$(MAKE) _db-reset DB_TARGET=$*
 
-.PHONE: _db-create-container
+.PHONY: _db-create-container
 _db-create-container:
 	@ask() { \
 		read -p "$$1 [y/N] " ans; \
@@ -90,23 +90,23 @@ _db-create-container:
 				-d $(DB_POSTGRES_IMAGE) || error "Failed to create PostgreSQL container"; \
 			;; \
 		*) \
-			error "DB type $$DB_TYPE not supported. Use 'mongo' or 'postgres'."; \
+			error "DB type $$DB_TYPE not supported. Use 'mongodb' or 'postgres'."; \
 			;; \
 	esac;
 	\
 	echo "DB container $$DB_CONTAINER_NAME created successfully on port $$DB_PORT"
 
-.PHONE: _db-migrate
+.PHONY: _db-migrate
 _db-migrate:
 	@DB_NAME=$(call get-db-var,$(DB_TARGET),DB_NAME); \
 	echo "Migrate Database $$DB_NAME"; \
 	go run cmd/migrations/main.go up $$DB_NAME;
 
-.PHONE: _db-drop
+.PHONY: _db-reset
 _db-reset:
-	DB_CONTAINER_NAME=$(call get-db-var,$(DB_TARGET),DB_CONTAINER_NAME); \
-	echo "Dropping Database $$DB_NAME"; \
-	echo "DB container $$DB_CONTAINER_NAME created successfully on port $$DB_PORT"
+	@DB_NAME=$(call get-db-var,$(DB_TARGET),DB_NAME); \
+	echo "Reset Database $$DB_NAME"; \
+	go run cmd/migrations/main.go reset $$DB_NAME;
 
 # Function to get database configuration - fixed version
 get-db-var = $(if $($1.$2),$($1.$2),$($2))
